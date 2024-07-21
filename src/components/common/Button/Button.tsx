eba6e4d580a6a139f1d3f7930d5a6acafab99267
Ramil <crouzch1@gmail.com>
2024-07-21 18:50:58 +0500

    fix: add onClick prop to Button

--- src/components/common/Button/Button.tsx
+++ src/components/common/Button/Button.tsx
@@ -6,17 +6,19 @@ interface ButtonProps {
 	type?: 'submit' | 'reset' | 'button';
 	text?: string;
 	className?: string;
+	onClick?: () => void;
 }
 
 const Button: React.FC<ButtonProps> = ({
 	text,
 	type = 'button',
 	className,
+	onClick,
 }) => {
 	const classNames = clsx(style.button, className);
 
 	return (
-		<button type={type} className={classNames}>
+		<button type={type} className={classNames} onClick={onClick}>
 			{text}
 		</button>
 	);

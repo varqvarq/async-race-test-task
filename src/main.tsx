71f16f2e1e8bccb9e8dc922f5e5c3bd5b3c6e2bb
Ramil <crouzch1@gmail.com>
2024-07-31 22:46:08 +0500

    feat: add routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,36 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import {
+	Navigate,
+	Route,
+	RouterProvider,
+	createBrowserRouter,
+	createRoutesFromElements,
+} from 'react-router-dom';
 
 import App from './App';
+import NotFound from './components/NotFound/NotFound';
+// import Header from './components/Header/Header';
+import Garage from './pages/Garage/Garage';
+import Winners from './pages/Winners/Winners';
+import store from './redux/store';
 
 import './index.css';
 
+const router = createBrowserRouter(
+	createRoutesFromElements(
+		<Route path='/' element={<App />} errorElement={<NotFound />}>
+			<Route index element={<Navigate to='/garage' />} />
+			<Route path='garage' element={<Garage />} />
+			<Route path='winners' element={<Winners />} />
+		</Route>
+	)
+);
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>
+	// <React.StrictMode>
+	<Provider store={store}>
+		<RouterProvider router={router} />
+	</Provider>
+	// </React.StrictMode>
 );
